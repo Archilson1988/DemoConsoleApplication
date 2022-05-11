599a6e9d8fdeb4e6324eb179995f9070f0ff2c99
Archilson1988 <105069417+Archilson1988@users.noreply.github.com>
2022-05-11 19:37:09 +0300

    Feutures/additional function (#1)
    * added aditional function
    
    * added sub functuion
    
    * remove file

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -3,6 +3,16 @@
 
 #include <iostream>
 
+int add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int sub(int num1, int num2)
+{
+    return num1 - num2;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
